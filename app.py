597d54458a63d0f9520381a55879d3eaa7b3138a
IvanKuzznetsov <ivan.kuzznetsov@gmail.com>
2024-04-15 13:47:30 +0700

    Добавлены отделы

--- app.py
+++ app.py
@@ -2,22 +2,60 @@ import os
 
 from flask import Flask, render_template
 
-from database import db, Employee
+from database import db, Employee, Department
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
+
+    development = Department(name="Разработка")
+    db.session.add(development)
+
+    hr = Department(name="Рекрутинг")
+    db.session.add(hr)
+
+    db.session.add(
+        Employee(
+            fullname="Мария Петрова",
+            department=development,
+        )
+    )
+    db.session.add(
+        Employee(
+            fullname="Петя Иванов",
+            department=development,
+        )
+    )
+    db.session.add(
+        Employee(
+            fullname="Тамара Сидорова",
+            department=hr,
+        )
+    )
+
+    db.session.commit()
 
 
 @app.route("/")
 def all_employees():
     employees = Employee.query.all()
     return render_template("all_employees.html", employees=employees)
+
+
+@app.route("/department/<int:department_id>")
+def employees_by_department(department_id):
+    department = Department.query.get_or_404(department_id)
+    return render_template(
+        "employees_by_department.html",
+        department_name=department.name,
+        employees=department.employees
+    )
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+from sqlalchemy.orm import relationship
 
 
 db = SQLAlchemy()
@@ -10,5 +11,18 @@ class Employee(db.Model):
     fullname = db.Column(db.String)
     added = db.Column(db.DateTime, nullable=False, default=func.now())
 
+    department_id = db.Column(db.Integer, db.ForeignKey("department.id", ondelete="SET NULL"))
+    department = relationship("Department", back_populates="employees")
+
     def __repr__(self):
         return f"User(fullname={self.fullname!r})"
+
+
+class Department(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    employees = relationship("Employee", back_populates="department")
+
+    def __repr__(self):
+        return f"Department(name={self.name!r})"
